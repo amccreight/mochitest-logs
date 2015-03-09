2c6aea62e8ba42ec67656018a6942af83e504c60
Andrew McCreight <continuation@gmail.com>
2015-03-09 11:43:51 -0700

    add script to print out information about DOM windows that leak

---
+++ plusplus.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Analyze the ++/-- DOMWINDOW in a TBPL log.
+
+import re
+import sys
+
+winPatt = re.compile('\d\d:\d\d:\d\d\W+INFO -  (..)DOMWINDOW == (\d+).*\[pid = (\d+)\] \[serial = (\d+)\]')
+
+
+#11:28:12     INFO -  ++DOMWINDOW == 1 (0xa7a75c00) [pid = 2238] [serial = 1] [outer = (nil)]
+
+live = set([])
+
+for l in sys.stdin:
+    m = winPatt.match(l)
+    if not m:
+        continue
+    isNew = m.group(1) == '++'
+    assert isNew or m.group(1) == '--'
+
+    numLive = int(m.group(2))
+    pid = int(m.group(3))
+    serial = int(m.group(4))
+
+    #print isNew, 'XXX serial=' , serial, 'XXX pid=', pid, 'XXX', l[:-1]
+
+    winId = (pid, serial)
+
+    if isNew:
+        live.add(winId)
+    else:
+        assert winId in live
+        live.remove(winId)
+
+
+# Print out informatio about leaking windows.
+for x in live:
+    print "[pid = {0}] [serial = {1}]".format(x[0], x[1])
+
+
