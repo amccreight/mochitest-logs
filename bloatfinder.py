4065bfd09554c214029b106cf5034b0c2e6a940a
Andrew McCreight <continuation@gmail.com>
2015-07-31 11:16:11 -0700

    print out windows per second chart

--- bloatfinder.py
+++ bloatfinder.py
@@ -8,10 +8,40 @@
 
 import sys
 import re
+import time
 
-winPatt = re.compile('\d\d:\d\d:\d\d\W+INFO -  (..)DOMWINDOW == (\d+).*\[pid = (\d+)\] \[serial = (\d+)\]')
+winPatt = re.compile('(\d\d:\d\d:\d\d)\W+INFO -  (..)DOMWINDOW == (\d+).*\[pid = (\d+)\] \[serial = (\d+)\]')
 
-# XXX count the number of live windows at once. just do it manually.
+def timeFromString(t):
+    [hr, mi, se] = t.split(':')
+    return ((60 * int(hr)) + int(mi)) * 60 + int(se)
+
+def stringFromTime(t0):
+    se = t0 % 60
+    t = t0 / 60
+    mi = t % 60
+    h = t / 60
+    return '{:0>2}:{:0>2}:{:0>2}'.format(h, mi, se)
+
+
+def windowsPerSecondChart(windowsPerSecond):
+    keys = sorted(windowsPerSecond.keys())
+    prevKey = keys[0] - 1
+    for k in keys:
+        while prevKey + 1 < k:
+            prevKey += 1
+            print stringFromTime(prevKey)
+        count = windowsPerSecond.get(k, 0)
+        sys.stdout.write(stringFromTime(k) + ' ')
+        i = 0
+        while i < count:
+            sys.stdout.write('*')
+            i += 1
+        print
+
+        prevKey = k
+
+
 
 test = None
 numLive = 0
@@ -21,6 +51,8 @@ peakNumLive = 0
 
 counts = {}
 peaks = {}
+
+windowsPerSecond = {}
 
 for l in sys.stdin:
     if l.find("TEST-START") > -1:
@@ -33,17 +65,22 @@ for l in sys.stdin:
     m = winPatt.match(l)
     if not m:
         continue
-    if m.group(1) == '++':
+    if m.group(2) == '++':
         count += 1
         numLive += 1
+        t = timeFromString(m.group(1))
+        windowsPerSecond[t] = windowsPerSecond.setdefault(t, 1) + 1
         if numLive > peakNumLive:
             peakNumLive = numLive
     else:
-        assert m.group(1) == '--'
+        assert m.group(2) == '--'
         assert numLive > 0
         numLive -= 1
 
 
+
+windowsPerSecondChart(windowsPerSecond)
+exit(0)
 
 keys = sorted(counts.keys())
 keys.reverse()
