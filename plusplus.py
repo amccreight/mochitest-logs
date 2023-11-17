27f028327a3da88d30f73068bfd43d68a6c06c0c
Andrew McCreight <continuation@gmail.com>
2023-11-17 11:07:02 -0800

    turn plusplus into a more fully fledged leak log analyser

--- plusplus.py
+++ plusplus.py
@@ -12,57 +12,156 @@ import sys
 winPatt = re.compile('.*I/DocShellAndDOMWindowLeak (..)DOMWINDOW == (\d+).*\[pid = (\d+)\] \[serial = (\d+)\] \[[^\]]+\].?(\[url = [^\]]+)?')
 urlLen = len('[url = ')
 
-#[task 2020-05-19T14:19:44.223Z] 14:19:44     INFO - GECKO(1230) | [(null) 1230: Main Thread]: I/DocShellAndDOMWindowLeak ++DOMWINDOW == 1 (0x7fc8cc397520) [pid = 1230] [serial = 1] [outer = (nil)]
 
-#[task 2020-05-19T14:19:59.248Z] 14:19:59     INFO - GECKO(1230) | [Child 1308: Main Thread]: I/DocShellAndDOMWindowLeak --DOMWINDOW == 3 (0x7f4be4689400) [pid = 1308] [serial = 2] [outer = (nil)] [url = about:blank]
+leakLogCreatePatt = re.compile('.*### XPCOM_MEM_BLOAT_LOG defined -- logging bloat/leaks to (.+)$')
+
+leakLogReadPatt = re.compile('.*INFO \- leakcheck \| Processing leak log file (.+)$')
+
+bloatViewPatt = re.compile('.*BloatView: ALL \(cumulative\) LEAK AND BLOAT STATISTICS\, ([a-zA-Z]+) process (\d+)$')
+
+failPatt = re.compile('.*TEST-UNEXPECTED-FAIL \| leakcheck large nsGlobalWindowInner \| (.+)$')
+
+testPatt = re.compile('.*INFO \- TEST-(START|SKIP|OK) \| (\S+)')
+
+windowCountPatt = re.compile('INFO \- TEST-INFO \| leakcheck \| [a-zA-Z]+ leaked ([0-9]+) (nsGlobalWindowInner|nsGlobalWindowOuter)')
 
 def findLeakers():
     live = {}
     hiddenLive = {}
     foundAny = False
+    lastLeakLog = None
+    currTest = None
+    inSkip = False
+    currBloatPid = 0
+    pidLeaks = {}
+    testsForLogs = {}
+    leakingLogs = {}
+
+    sys.stdin.reconfigure(encoding='latin1')
 
     for l in sys.stdin:
-        m = winPatt.search(l)
-        if not m:
+        tp = testPatt.search(l)
+        if tp:
+            state = tp.group(1)
+            t = tp.group(2)
+            if state == "START":
+                if t == "Shutdown":
+                    continue
+                assert (currTest is None) or inSkip
+                currTest = t
+                inSkip = False
+            elif state == "SKIP":
+                assert currTest == t
+                inSkip = True
+            elif state == "OK":
+                assert currTest == t
+                currTest = None
+                inSkip = False
             continue
 
-        isNew = m.group(1) == '++'
-        assert isNew or m.group(1) == '--'
+        m = winPatt.search(l)
+        if m:
+            isNew = m.group(1) == '++'
+            assert isNew or m.group(1) == '--'
 
-        numLive = int(m.group(2))
-        pid = int(m.group(3))
-        serial = int(m.group(4))
-        url = m.group(5)
-        if url:
-            url = url[urlLen:]
+            numLive = int(m.group(2))
+            pid = int(m.group(3))
+            serial = int(m.group(4))
+            url = m.group(5)
+            if url:
+                url = url[urlLen:]
 
-        winId = (pid, serial)
+            winId = (pid, serial)
 
-        if isNew:
-            assert url is None
+            if isNew:
+                assert url is None
 
-            if winId in live:
-                assert not winId in hiddenLive
-                hiddenLive[winId] = None
+                if winId in live:
+                    assert not winId in hiddenLive
+                    hiddenLive[winId] = currTest
+                else:
+                    live[winId] = currTest
+                foundAny = True
             else:
-                live[winId] = None
-            foundAny = True
-        else:
-            assert winId in live
-            del live[winId]
+                assert winId in live
+                del live[winId]
+            continue
+
+        f = failPatt.search(l)
+        if f:
+            leakLog = f.group(1)
+            assert not leakLog in leakingLogs
+            leakingLogs[lastLeakLog] = f.group(1)
+            continue
+
+        llc = leakLogCreatePatt.search(l)
+        if llc:
+            leakLog = llc.group(1)
+            t = currTest if currTest else "NONE"
+            testsForLogs.setdefault(leakLog, []).append(t)
+            continue
+
+        llr = leakLogReadPatt.search(l)
+        if llr:
+            lastLeakLog = llr.group(1)
+            continue
+
+        bv = bloatViewPatt.search(l)
+        if bv:
+            # bv.group(1) is the log file.
+            currBloatPid = int(bv.group(2))
+
+            # XXX Hopefully if a PID is reused, both won't leak.
+            assert not currBloatPid in pidLeaks
+            pidLeaks[currBloatPid] = {}
+
+            continue
+
+        wc = windowCountPatt.search(l)
+        if wc:
+            count = int(wc.group(1))
+            which = wc.group(2)
+            m = pidLeaks[currBloatPid]
+            assert not which in m
+            m[which] = count
+            continue
 
 
     if not foundAny:
         print("Didn't find any windows in the log.")
 
-    # Print out information about leaking windows.
-    print("HIDDEN")
-    for x, url in hiddenLive.items():
-        print("[pid = {0}] [serial = {1}] URL was {2}".format(x[0], x[1], url))
+    pidToWindows = {}
+    for x, currTest in hiddenLive.items():
+        pid = x[0]
+        pidToWindows.setdefault(pid, []).append([x[0], x[1], currTest])
+    for x, currTest in live.items():
+        pid = x[0]
+        pidToWindows.setdefault(pid, []).append([x[0], x[1], currTest])
 
-    print("OTHER")
-    for x, url in live.items():
-        print("[pid = {0}] [serial = {1}] URL was {2}".format(x[0], x[1], url))
+    nonDupeCount = 0
+    for log, tests in testsForLogs.items():
+        if len(tests) == 1:
+            nonDupeCount += 1
+            continue
+        tstring = "\n  ".join(tests)
+        print(f'DUPLICATE LOGS {log}:\n  {tstring}')
+    print(f'NUMBER OF NON-DUPLICATE LOGS: {nonDupeCount}')
+    print()
+
+    for pid, m in pidLeaks.items():
+        print(f'pid:{pid}')
+        print('  leaked: ', end='')
+        for which, count in m.items():
+            print(f'{count} {which}, ', end='')
+        print()
+        for [x, y, z] in pidToWindows.get(pid, []):
+            print(f'  [pid = {x}] [serial = {y}] during {z}')
+    print()
+
+    for log, reported in leakingLogs.items():
+        print(f'log: {log}')
+        print(f'  reported test directory: {reported}')
+        print(f'  actual test: {", ".join(testsForLogs.get(log, ["NONE"]))}')
 
 
 findLeakers()
