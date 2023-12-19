7c3023cbc9c080596b9028c5227c96f92240e057
Andrew McCreight <continuation@gmail.com>
2023-12-19 07:20:44 -0800

    remove a comment from line_split

--- line_split.py
+++ line_split.py
@@ -10,8 +10,6 @@ import re
 endLen = len("[task 2023-11-02T16:59:39.586Z] 16:59:39     INFO - ")
 splitRe = re.compile("WARNING: rejected message: (.*)$")
 
-# [task 2023-03-07T22:45:40.818Z] 22:45:40     INFO - PID 29233 | [Parent 29233, Main Thread] WARNING: useless message: MarionetteEvents : MarionetteEventsChild:PageLoadEvent: file /builds/worker/checkouts/gecko/dom/ipc/jsactor/JSActorManager.cpp:214
-
 for l in sys.stdin:
     print(l[endLen:-1])
     continue
